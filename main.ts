f34b5931e52218e1b4bf65ea5ad3c354a78d2ae7
pc-ccwa <115084257+pc-ccwa@users.noreply.github.com>
2022-10-26 15:31:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
 let x = 0
 let y = 0
 input.onButtonPressed(Button.A, function () {
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
+    basic.clearScreen()
+    x = 0
+    y = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
             led.plot(x, y)
             basic.pause(200)
-            basic.clearScreen()
+            x += 1
         }
+        y += 1
+        x = 0
     }
 })
 input.onButtonPressed(Button.B, function () {
