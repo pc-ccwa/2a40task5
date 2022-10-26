8dd5a26f1e176ec0c92890c884171848814862df
pc-ccwa <115084257+pc-ccwa@users.noreply.github.com>
2022-10-26 15:24:26 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
+let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
-    for (let i = 0; i <= 4; i++) {
-        for (let j = 0; j <= 4; j++) {
-            led.plot(j, i)
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
             basic.pause(200)
             basic.clearScreen()
-            if (i == 0 && j == 4) {
-                i += 1
-            }
-            while (false) {
-            	
-            }
         }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    x = 0
+    y = 0
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            basic.pause(200)
+            x += 1
+        }
+        y += 1
+        for (let index = 0; index < 5; index++) {
+            x += -1
+            led.plot(x, y)
+            basic.pause(200)
+        }
+        y += 1
     }
 })
 basic.forever(function () {
